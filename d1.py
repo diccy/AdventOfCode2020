2ef231c7f4868dd0cac29ddfd22bc1ab65ba24e8
Djobi <diccy91@hotmail.com>
2020-12-07 19:58:37 +0100

    rework d1 optimization

--- d1.py
+++ d1.py
@@ -1,22 +1,34 @@
+from copy import copy
+
 map = {}
 with open("d1.txt", "r") as f:
     for s in f.read().splitlines():
         map[int(s)] = True
 
 def PartOne(map):
-    for i in map.keys():
-        diff = 2020 - i
+    for n in map.keys():
+        diff = 2020 - n
         if map.get(diff) != None:
-            print("Bon gaulois 1:", i * diff)
+            print("Bon gaulois 1:", n * diff)
             return
 
 def PartTwo(map):
-    for i in map.keys():
-        for j in map.keys():
-            diff = 2020 - (i + j)
-            if map.get(diff) != None:
-                print("Bon gaulois 2:", i * j * diff)
-                return
+    nit = iter(map)
+    while True:
+        try:
+            n = next(nit)
+            mit = copy(nit)
+            while True:
+                try:
+                    m = next(mit)
+                    diff = 2020 - (n + m)
+                    if map.get(diff) != None:
+                        print("Bon gaulois 2:", n * m * diff)
+                        return
+                except StopIteration:
+                    break
+        except StopIteration:
+            break
 
 PartOne(map)
 PartTwo(map)
