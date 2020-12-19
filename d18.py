96c63d3d5c29efb0fb0ddf4ae01091cb58bbb2e9
Djobi <diccy91@hotmail.com>
2020-12-19 01:43:04 +0100

    d18

---
+++ d18.py
@@ -0,0 +1,58 @@
+
+def Resolve(do_print = False):
+
+    with open('d18.txt', 'r') as f:
+        content = f.read().replace(' ', '').splitlines()
+
+    ORD_0  = ord('0')
+    ORD_P  = ord('+')
+    ORD_M  = ord('*')
+    ORD_PO = ord('(')
+    ORD_PC = ord(')')
+    OP_P = lambda a, b: a + b
+    OP_M = lambda a, b: a * b
+
+    #              +  *
+    PRIORITIES1 = (1, 1)
+    PRIORITIES2 = (2, 1)
+
+    def Evaluate(line, prio):
+        values = []
+        ops = []
+        for c in line:
+            o = ord(c)
+            if o == ORD_P or o == ORD_M:
+                op, p = (OP_P, prio[0]) if o == ORD_P else (OP_M, prio[1])
+                if len(ops) > 0:
+                    while len(ops) > 0 and ops[-1][1] >= p:
+                        values.append(ops.pop()[0](values.pop(), values.pop()))
+                ops.append((op, p))
+            elif o == ORD_PO:
+                ops.append((ORD_PO, 0))
+            elif o == ORD_PC:
+                while ops[-1][0] != ORD_PO:
+                    values.append(ops.pop()[0](values.pop(), values.pop()))
+                ops.pop()
+            else:
+                values.append(o - ORD_0)
+
+        while len(values) > 1:
+            values.append(ops.pop()[0](values.pop(), values.pop()))
+        return values[0]
+
+    sum1 = 0
+    sum2 = 0
+    for line in content:
+        sum1 += Evaluate(line, PRIORITIES1)
+        sum2 += Evaluate(line, PRIORITIES2)
+
+    if do_print:
+        print('Somme gauloise 1:', sum1)
+        print('Somme gauloise 2:', sum2)
+
+# #############################################################################
+if __name__ == '__main__':
+    Resolve(True)
+    # Results with given input:
+    #   1: 36382392389406
+    #   2: 381107029777968
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 import  d1,  d2,  d3,  d4,  d5,  d6,  d7,  d8,  d9, d10
-import d11, d12, d13, d14, d15, d16, d17#, d18, d19, d20
+import d11, d12, d13, d14, d15, d16, d17, d18#, d19, d20
 #import d21, d22, d23, d24
 
 from time import time
@@ -25,7 +25,7 @@ def main():
         'd15': d15,
         'd16': d16,
         'd17': d17,
-        #'d18': d18,
+        'd18': d18,
         #'d19': d19,
         #'d20': d20,
         #'d21': d21,
